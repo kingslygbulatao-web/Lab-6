6351f32945de8595f3494aac79d4a054fd45a331
Me <planetaryaxis93@gmail.com>
2026-02-15 14:29:53 -0800

    1st progress

--- main.cpp
+++ main.cpp
@@ -6,3 +6,32 @@ int main(){
 
     return 0;
 }
+
+int EnterArrayData(double &, double &);
+
+int EnterArrayData(double &a, double &b){
+
+    double recievedinput;
+    int AmountofTimes;
+    int ElementInput;
+
+    for(int i = 0; i < AmountofTimes + 1; i++){
+
+    std::cout<< "Data Entry for the array:" << std::endl;
+
+    std::cout<< "Element #" << AmountofTimes << ": " << ElementInput << std::endl;
+
+    }
+
+    return 0;
+}
+
+int OutputArrayData(){
+
+    return 0;
+}
+
+int sumarray(){
+
+    return 0;
+}
